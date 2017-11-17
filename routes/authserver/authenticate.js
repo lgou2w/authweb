34e333d2b86de70b9e1d2caa4c5b251cfb465b02
MoonLake <mcmoonlake@hotmail.com>
2017-11-17 20:59:48 +0800

    Authenticate function is done and debugged, but the code needs to be optimized

--- routes/authserver/authenticate.js
+++ routes/authserver/authenticate.js
@@ -19,6 +19,7 @@ var authErrorRes = require('../../util/authError').authErrorRes;
 var authError = require('../../util/authError').authError;
 var security = require('../../util/security');
 var uuid = require('../../util/uuid');
+var utils = require('../../util/utils');
 
 /**
  * POST request when the client uses a username and password for authentication.
@@ -84,7 +85,7 @@ function validateAndQueryProfile(req, res, mysql, user, values, callback) {
         return;
     }
     // Interception of the password and salt to compare the hash value of the user's plain text password. If not, then invalid password
-    // 截取密码和salt以比较用户纯文本密码的哈希值.  如果不符合, 则密码无效
+    // 截取密码和 salt 以比较用户纯文本密码的哈希值.  如果不符合, 则密码无效
     var password = values[0].password;
     var salt = password.substr(0, password.indexOf('$'));
     var passwordHash = password.substr(password.indexOf('$') + 1);
@@ -97,37 +98,7 @@ function validateAndQueryProfile(req, res, mysql, user, values, callback) {
     // Generated if the client does not provide a clientToken
     // 如果客户端未提供客户端令牌则生成
     if(clientToken === undefined) {
-        // Detects the maximum number of access tokens. If the limit, then login failed
-        // 检测最大访问令牌数量. 如果限制, 则登录失败
-        var tokenMaxCount = req.app.get('config').token.maxCount;
-        mysql.query('select `id` from `user-token` where binary `userId`=? limit ?;', [user.uuid, tokenMaxCount], function (err, values) {
-            if(err) {
-                callback(err);
-                return;
-            }
-            // Login failed if limit token has been exceeded
-            // 如果超过限制令牌, 则登录失败
-            if(values.length >= tokenMaxCount) {
-                authErrorRes(res, 'ForbiddenOperationException', 'Invalid clientToken. The maximum number of accessToken.', 403);
-                return;
-            }
-            // Otherwise, a new token is generated and stored
-            // 否则生成并存储新的令牌
-            var accessToken = uuid.random(true);
-            clientToken = uuid.random(true);
-            mysql.query('insert into `user-token`(`accessToken`,`clientToken`,`userId`,`timestamp`) values(?,?,?,?);', [
-                accessToken,
-                clientToken,
-                user.uuid,
-                new Date().getTime()
-            ], function (err) {
-                if(err) {
-                    callback(err);
-                } else {
-                    callback(null, buildUserProfile(accessToken, clientToken, user));
-                }
-            });
-        });
+        buildTokenAndUserProfile(req, res, mysql, user, true, callback);
     } else {
         // Query the accessTokens for the given clientToken and user id
         // 查询给定的客户端令牌和用户ID的访问令牌
@@ -140,22 +111,72 @@ function validateAndQueryProfile(req, res, mysql, user, values, callback) {
         }
         // Otherwise, the format of clientToken is correct, then query
         // 否则客户端令牌的格式正确, 则查询
-        mysql.query('select `accessToken`,`timestamp` from `user-token` where binary `clientToken`=? and binary `userId`=? limit 1;', [
+        mysql.query('select `accessToken` from `user-token` where binary `clientToken`=? and binary `userId`=? limit 1;', [
             clientToken,
             user.uuid
         ], function (err, values) {
             if(err) {
                 callback(err);
             } else {
-                // TODO validate accessToken
+                // If the length of 0 indicates that the given clientToken does not have an accessToken, a new one is generated
+                // 如果长度为 0 表示给定的客户端令牌没有访问令牌, 则会生成一个新的
                 if(values.length === 0) {
+                    buildTokenAndUserProfile(req, res, mysql, user, false, callback);
+                } else {
+                    // Otherwise, return user profile
+                    // 否则返回用户档案
+                    var accessToken = values[0].accessToken;
+                    callback(null, buildUserProfile(accessToken, clientToken, user));
                 }
-                var accessToken = values[0].accessToken;
-                var timestamp = values[0].timestamp;
-                callback(null, values);
             }
         });
     }
+}
+
+function buildTokenAndUserProfile(req, res, mysql, user, check, callback) {
+    if(!check) {
+        // If not check, a new token is generated and stored
+        // 如果不检测, 则生成并存储新的令牌
+        buildToken(mysql, user, callback);
+    } else {
+        // Otherwise, detects the maximum number of access tokens. If the limit, login is invalid
+        // 否则检测最大访问令牌数量. 如果限制, 则登录无效
+        var tokenMaxCount = req.app.get('config').token.maxCount;
+        mysql.query('select `id` from `user-token` where binary `userId`=? limit ?;', [user.uuid, tokenMaxCount], function (err, values) {
+            if(err) {
+                callback(err);
+                return;
+            }
+            // Login invalid if limit token has been exceeded
+            // 如果超过限制令牌, 则登录无效
+            if(values.length >= tokenMaxCount) {
+                authErrorRes(res, 'ForbiddenOperationException', 'Invalid clientToken. The maximum number of accessToken.', 403);
+                return;
+            }
+            // Otherwise, a new token is generated and stored
+            // 否则生成并存储新的令牌
+            buildToken(mysql, user, callback);
+        });
+    }
+}
+
+function buildToken(mysql, user, callback) {
+    // A new token is generated and stored
+    // 生成并存储一个新的令牌
+    var accessToken = uuid.random(true);
+    var clientToken = uuid.random(true);
+    mysql.query('insert into `user-token`(`accessToken`,`clientToken`,`userId`,`timestamp`) values(?,?,?,?);', [
+        accessToken,
+        clientToken,
+        user.uuid,
+        utils.timestamp()
+    ], function (err) {
+        if(err) {
+            callback(err);
+        } else {
+            callback(null, buildUserProfile(accessToken, clientToken, user));
+        }
+    });
 }
 
 function buildUserProfile(accessToken, clientToken, user) {
